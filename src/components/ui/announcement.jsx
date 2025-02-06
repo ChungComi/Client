99490974720dbe1d26e2057c9130f6be218f11db
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-06 15:39:01 +0900

    공지사항 컴포넌트 구현

---
+++ src/components/ui/announcement.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./css/anouncement.css"
+
+const Announcement = () => {
+    return (
+        <div>
+            <div className="rounded-box">
+                <div className="button-container">
+                    <button>학교</button>
+                    <button>학과</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Announcement;

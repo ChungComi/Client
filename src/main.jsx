fc195be4bb9821436e6febdb92f4321b7e8bfbd7
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-06 13:58:39 +0900

    main

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ChoongBoong from "./screen/chungboong.jsx";
+
+ReactDOM.createRoot(document.querySelector("#app")).render(
+    <React.StrictMode>
+        <div>
+            <ChoongBoong></ChoongBoong>
+        </div>
+    </React.StrictMode>
+);

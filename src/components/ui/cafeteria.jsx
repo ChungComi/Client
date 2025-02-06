b74972a2adcb7b15c98bc4e400a09bd15618856a
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-06 14:01:10 +0900

    식단표 컴포넌트

---
+++ src/components/ui/cafeteria.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./css/cafeteria.css";
+
+const Cafeteria = () => {
+    return (
+        <div>
+            <div className="rounded-box">
+                <div className="button-container">
+                    <button>양진재</button>
+                    <button>양성재</button>
+                    <button>본관</button>
+                    <button>1층</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cafeteria;

a1e4a34b9ab844eb1b5345f3abc8db4628bed134
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-06 14:53:53 +0900

    app.jsx로 화면 이동 조절

---
+++ src/components/ui/app.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../../screen/login.jsx";
+import Chungboong from "../../screen/chungboong.jsx";
+import Register from "../../screen/register.jsx";
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/home" element={<Chungboong/>} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;

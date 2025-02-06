e44887de809b2a7143416fb95e53bd145eaa8ba8
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-06 15:40:08 +0900

    하단 바랑 공지사항 컴포넌트 추가

--- src/screen/chungboong.jsx
+++ src/screen/chungboong.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Calendar from "../components/ui/calendar.jsx";
 import Timetable from "../components/ui/timetable.jsx";
 import Cafeteria from "../components/ui/cafeteria.jsx";
+import Announcement from "../components/ui/announcement.jsx";
 
 const ChoongBoong = ()=>{
     return (
@@ -9,6 +10,7 @@ const ChoongBoong = ()=>{
             <Calendar></Calendar>
             <Timetable></Timetable>
             <Cafeteria></Cafeteria>
+            <Announcement></Announcement>
         </div>
     )
 }

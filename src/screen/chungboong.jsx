6d2bf430d04d63bf63f7e455b008a215075936d4
jeongseonwoong <tjsdnd1219@naver.com>
2025-02-06 13:59:17 +0900

    식단표, 일정관리 달력, 시가눂가 있는 화면

---
+++ src/screen/chungboong.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Calendar from "../components/ui/calendar.jsx";
+import Timetable from "../components/ui/timetable.jsx";
+import Cafeteria from "../components/ui/cafeteria.jsx";
+
+const ChoongBoong = ()=>{
+    return (
+        <div>
+            <Calendar></Calendar>
+            <Timetable></Timetable>
+            <Cafeteria></Cafeteria>
+        </div>
+    )
+}
+
+export default ChoongBoong
